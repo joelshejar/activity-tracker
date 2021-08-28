3a9d07fb03269446af8e9c1a7f64b1f1f3ba6af6
joelshejar <joel.rajesh13@gmail.com>
2021-08-28 12:35:37 +0530

    Added Styling

---
+++ code/src/components/Activity.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Activity extends React.Component{
+    render(){
+        return(
+            <section className='activity'>
+                <div className='month'>
+                    <h2>Random</h2>
+                    <div>
+                        <h5>August</h5>
+                    </div>
+                </div>
+                <div className='days'>
+
+                </div>
+                <div className='cross'>
+                    <div>X</div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Activity
--- code/src/components/App.js
+++ code/src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Activity from './Activity'
+import '../styles/index.css'
 
 class App extends React.Component{
     constructor(props){
@@ -8,7 +10,20 @@ class App extends React.Component{
         }
     }
     render(){
-        return
+        return(
+            <>
+                <header>
+                    <h1>Monthly Activity Tracker!</h1>
+                    <form>
+                        <input type='text' placeholder='e.g. coding'></input>
+                        <button>Add Activity</button>
+                    </form>
+                </header>
+                <main>
+                    <Activity/>
+                </main>
+            </>
+        )
     }
 }
 
